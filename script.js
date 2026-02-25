dd730a6ed4cd0dc59281d62135b9ae1f0ede39fb
Malik <mrogers@icstars.org>
2026-02-25 12:08:46 -0600

    added Cards

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ async function searchbooks(query){
     const data = await response.json();
     displaybooks(data.docs);
 }
+
+function displaybooks(){
+    items.innerHTML=``;
+    books.slice(0,10).foreach(book=>{
+        const card = document.createElement('div');
+        card.classList.add('title');
+        card.innerHTML = 
+        ` <h2>${book.title}</h2>
+            <p><strong>Author:</strong>${book.author_name?.[0] ?? 'unknown'}</p>
+            <p><strong>first published:</strong>${book.first_published_year ?? '?'}</p>`;
+            items.appendchild(card);
+    })
+}
