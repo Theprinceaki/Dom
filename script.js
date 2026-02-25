0fa627bb76281af6e9b5061682a818b73a815dd2
Malik <mrogers@icstars.org>
2026-02-25 11:44:07 -0600

    Added an API

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-const title=document.getElementById('title');
-const card=document.getElementById('card');
-const items=document.getElementById('item');
+const title=document.querySelector('title');
+const input=document.querySelector('input');
+const items=document.querySelectorAll('item');
 
+input.addEventListener('keyup', ()=>{
+    const query = input.value.trim();
+    if(query.length > 2){
+        searchbooks(query);
+    }
+});
+
+
+async function searchbooks(query){
+    const url=`https://openlibrary.org/search.json?q${encodeURIComponent(query)}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    displaybooks(data.docs);
+}
